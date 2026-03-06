73ba4f8d36a3f19d228fa7278183ad0911760e05
Tashan Appuhami <appuhamitashan@gmail.com>
2026-03-06 20:11:00 +0530

    signUp page UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './page/login/login';
+import { SignUp } from './page/sign-up/sign-up';
 
 export const routes: Routes = [
     {
         path: "login",
         component: Login
+    },
+    {
+        path: "signUp",
+        component: SignUp
     },
 
 ];
