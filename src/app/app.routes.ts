7c2c5d1f732ff44306d7bb1779dca797d6e539d3
Tashan Appuhami <appuhamitashan@gmail.com>
2026-03-06 20:00:46 +0530

    login_page UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './page/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: Login
+    },
+
+];
