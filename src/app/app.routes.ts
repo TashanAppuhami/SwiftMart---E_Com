f2f75b71e0640b1ca2ebe709e50f3a45e404acf7
Tashan Appuhami <appuhamitashan@gmail.com>
2026-03-09 14:31:22 +0530

    navbar UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './page/login/login';
 import { SignUp } from './page/sign-up/sign-up';
+import { LandingPage } from './page/landing-page/landing-page';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,9 @@ export const routes: Routes = [
         path: "signUp",
         component: SignUp
     },
+    {
+        path: "",
+        component: LandingPage
+    }
 
 ];
